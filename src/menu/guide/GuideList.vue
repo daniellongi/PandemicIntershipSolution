ec10b9d4e8597de413f148f185eef8c23621d90b
daniellongi <djvildosola@uc.cl>
2023-01-04 14:48:09 -0300

    guides ready

--- src/menu/guide/GuideList.vue
+++ src/menu/guide/GuideList.vue
@@ -7,7 +7,7 @@
     <ListView>
       <p v-if="loading" >Loading tours...</p>
       <p v-if="error">{{ error.message }}</p>
-      <GuideItem  v-if="tourGuideExample" v-for="(item, index) in tourGuideExample" :key="index" :item="item" />
+      <GuideItem  v-if="guides" v-for="(item, index) in guides" :key="index" :item="item" />
     </ListView>
   </div>
 </template>
@@ -33,10 +33,7 @@
       const { guides, loading, error }  = storeToRefs(useGuideStore())
       const { fetchGuides }             = useGuideStore()
       fetchGuides()
-
-      const tourGuideExample = [{"id":1,"guidename":"Daniel Huasherr","bio":"Hi, I am Daniel. Originally, from New York, I moved to London as a student a few years down the road.","avatar":"https://imagedelivery.net/y4DRrv32Tll84ztsvd9nJQ/6cefa42b-1b9a-4cc3-e665-70373f5d0400/public","country":"England","rating":10}]
-
-      return { tourGuideExample , loading , error }
+      return { guides , loading , error }
   },
   methods: {
 
